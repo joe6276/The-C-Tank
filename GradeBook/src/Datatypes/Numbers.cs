77c04d7ca057570ef87c6661e4ec152af0fc24a7
Jonathan Ndambuki <joendambuki16@gmail.com>
2023-01-04 17:30:37 +0300

    worked on Literals

---
+++ GradeBook/src/Datatypes/Numbers.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src.Datatypes
+{
+    public class Numbers
+    {
+        short i1 = 2453;
+// int i2 = 34;
+// long i3 = 45;
+// Console.WriteLine("tHE Short");
+// Console.WriteLine($" the Minimum Value is  {short.MinValue}");
+// Console.WriteLine($" the Minimum Value is  {short.MaxValue}");
+// Console.WriteLine($" the Minimum size is  {sizeof(short)}");
+
+
+// Console.WriteLine("tHE INt");
+// Console.WriteLine($" the Minimum Value is  {int.MinValue}");
+// Console.WriteLine($" the Minimum Value is  {int.MaxValue}");
+// Console.WriteLine($" the Minimum size is  {sizeof(int)}");
+
+
+// Console.WriteLine("tHE LoNG");
+// Console.WriteLine($" the Minimum Value is  {long.MinValue}");
+// Console.WriteLine($" the Minimum v is  {long.MaxValue}");
+// Console.WriteLine($" the Minimum size is  {sizeof(long)}");
+
+
+// Console.WriteLine("tHE LoNG");
+// Console.WriteLine($" the Minimum size is  {sizeof(Single)}");
+// Console.WriteLine($" the Minimum Value is  {Single.MinValue}");
+// Console.WriteLine($" the Minimum v is  {Single.MaxValue}");
+
+// Console.WriteLine("tHE Double");
+// Console.WriteLine($" the Minimum size is  {sizeof(Double)}");
+// Console.WriteLine($" the Minimum Value is  {Double.MinValue}");
+// Console.WriteLine($" the Minimum v is  {Double.MaxValue}");
+
+// Console.WriteLine("tHE Decimal");
+// Console.WriteLine($" the Minimum size is  {sizeof(Decimal)}");
+// Console.WriteLine($" the Minimum Value is  {Decimal.MinValue}");
+// Console.WriteLine($" the Minimum v is  {Decimal.MaxValue}");
+
+    }
+}
